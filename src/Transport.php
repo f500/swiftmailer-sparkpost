290bce18cc8297ada342900055d396b1da30d885
Michael O'Connell <wundbread@gmail.com>
2022-01-07 09:57:21 -0600

    update to guzzle 7

--- src/Transport.php
+++ src/Transport.php
@@ -8,7 +8,7 @@ namespace SwiftSparkPost;
 
 use Exception as AnyException;
 use GuzzleHttp\Client as GuzzleClient;
-use Http\Adapter\Guzzle6\Client as GuzzleAdapter;
+use Http\Adapter\Guzzle7\Client as GuzzleAdapter;
 use SparkPost\SparkPost;
 use SparkPost\SparkPostResponse;
 use Swift_DependencyContainer;
